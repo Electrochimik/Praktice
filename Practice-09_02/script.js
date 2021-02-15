23fcdb1aa9e08877aea5a5ffcc7337d7c09429b7
Electrochimik <vaschenkosash@gmail.com>
2021-02-15 20:32:06 +0200

    222

---
+++ Practice-09_02/script.js
@@ -0,0 +1,46 @@
+// -- створити об'єкт (не меньше 5ти властивостей) який описує
+// - собаку
+let dog = {
+    nick: `Topic`,
+    breed: `cur`,
+    age: 3,
+    adult: `male`,
+    color: `brown`
+}
+
+// - людину
+let human = {
+    name: `Petro`,
+    soname: `Petrenko`,
+    age: 32,
+    adult: `male`,
+    aducation: `hight`
+}
+
+// - автомобіль
+let car = {
+    model: `Octavia`,
+    brand: `Shkoda`,
+    year: 2008,
+    color: `grey`,
+    engineVolume: 1.8
+}
+
+// - квартиру
+let flat = {
+    quantity: 3,
+    floor: 18,
+    year: 2000,
+    material: `brick`,
+    district: `Darnytsya`
+}
+
+// - книгу
+let car = {
+    model: `Octavia`,
+    brand: `Shkoda`,
+    year: 2008,
+    color: `grey`,
+    engineVolume: 1.8
+}
+console.log(dog);
