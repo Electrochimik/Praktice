a98edc41a02e45ed25ebe993ed8ab18b50efd289
Electrochimik <vaschenkosash@gmail.com>
2021-03-21 23:54:05 +0200

    pr

---
+++ bonus-lesson1/src/components/user.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class User extends Component {
+
+    constructor(props) {
+        super(props);
+        let {user} = props;
+        this.state = {user}
+    }
+
+    render() {
+        let {user} =  this.props;
+        console.log(user);
+
+        return (
+            <div>
+                    <h4>{user.name}</h4>
+                    <p>{user.age} - {user.status.toString()} </p>
+
+                <button onClick={()=> {
+                    user.status = !user.status;
+                    this.setState({user})
+                }
+                }
+                >Change Status</button>
+            </div>
+        );
+    }
+}
+
+export default User;
